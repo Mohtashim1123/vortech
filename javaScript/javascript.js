1c9c540830090b9392a03a8382d33be03383ea61
mohtashim <mouhtashim5@gmail.com>
2018-09-24 22:09:51 -0700

    task3 include

---
+++ javaScript/javascript.js
@@ -0,0 +1,24 @@
+// alert("file attached");
+// var a = 1;
+// var b = "ali";
+// var c = abbas; 
+// var  a=1 , b=3 , c=5;
+// var a=4;
+// var b = 9;
+// var result=a+b;
+// alert(result);
+
+
+
+
+// var a =50;
+// var b = 20;
+// if(a<b)
+// {
+//     var result= a + b;
+//     document.write("a less than b:",result);
+// }else{
+//     document.write("invaild value");
+// }
+
+
