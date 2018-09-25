caab84f8b563b0aa8f0f96d1fc0c2295d84a785b
mohtashim <mouhtashim5@gmail.com>
2018-09-25 10:20:01 -0700

    nav pending

--- task 3/js.js
+++ task 3/js.js
@@ -1,13 +1,31 @@
+// scroll color change
 $(window).scroll(function(){
-	$('nav').toggleClass('scrolled', $(this).scrollTop() > 50);
+	$('nav').toggleClass('scrolled', $(this).scrollTop() > 100);
+});
+
+$(window).scroll(function(){
+	$('.logochange').toggleClass('scrolled', $(this).scrollTop() < 100);
 });
 
 
+
+
+// shadow
 $(window).scroll(function() {
-    if ($(window).scrollTop() > 500) {
-        $('#navBar').addClass('floatingNav');
-    } else {
-        $('#navBar').removeClass('floatingNav');
-    }
+	if ($(window).scrollTop() > -1) {
+		$('#navBar').addClass('floatingNav');
+	} else {
+		$('#navBar').removeClass('floatingNav');
+	}
 });
 
+// scroll nav hide and show
+var prev = 0;
+var $window = $(window);
+var nav = $('.scrollhide-nav');
+
+$window.on('scroll', function(){
+	var scrollTop = $window.scrollTop();
+	nav.toggleClass('hidden', scrollTop > prev);
+	prev = scrollTop;
+});
