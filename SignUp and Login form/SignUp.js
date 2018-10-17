dcb4fc69a77b9bdd80a9e20209f7d14ab90f8742
Mohtashim <mouhtashim5@gmail.com>
2018-10-17 16:15:44 -0700

    today validation work

--- SignUp and Login form/SignUp.js
+++ SignUp and Login form/SignUp.js
@@ -1,17 +1,11 @@
 
-
-
-
-
 function signUp1() {
-    console.log('hello')
-    // e.preventDefault();
-    let FirstName = document.getElementById('FirstName').value;
-    let Lname = document.getElementById('LastName').value;
-    let Uname = document.getElementById('UserName').value;
-    let Email = document.getElementById('Email').value;
-    let password = document.getElementById('password').value;
-    let Cpassword = document.getElementById('Cpassword').value;
+    let FirstName = document.getElementById('FirstName');
+    let Lname = document.getElementById('LastName');
+    let Uname = document.getElementById('UserName');
+    let Email = document.getElementById('Email');
+    let password = document.getElementById('password');
+    let Cpassword = document.getElementById('Cpassword');
     let fnameerror = document.getElementById('F_Name_Error');
     let Lnameerror = document.getElementById('L_Name_Error');
     let Unameerror = document.getElementById('U_Name_Error');
@@ -24,122 +18,113 @@ function signUp1() {
 
 
     // fname
-    if (FirstName == "") {
+    if (FirstName.value == "") {
         load.classList.add("hide");
         fnameerror.innerHTML = "**Plz fill First name Field**";
-        return false;
-    }
-    else if (FirstName !== "") {
-        fnameerror.innerHTML = "";
+        // return false;
     }
 
-    if ((FirstName.length <= 3) || FirstName.length > 20) {
+
+    else if ((FirstName.value.length <= 3) || FirstName.value.length > 20) {
         load.classList.add("hide");
         fnameerror.innerHTML = "** Fname must be 3 to 20 Character **";
-        return false;
+        // return false;
     }
 
-    if (!isNaN(FirstName)) {
+    else if (!isNaN(FirstName.value)) {
         load.classList.add("hide");
         fnameerror.innerHTML = "** Only Character Allow**";
-        return false;
+        // return false;
     }
 
     // Lname
-    if (Lname == "") {
+    else if (Lname.value == "") {
         load.classList.add("hide");
         Lnameerror.innerHTML = "**Plz fill Last name Field**";
-        return false;
-    }
-    else if (Lname !== "") {
-        Lnameerror.innerHTML = "";
+        // return false;
     }
 
-    if ((Lname.length <= 3) || Lname.length > 20) {
+    else if ((Lname.value.length <= 3) || Lname.value.length > 20) {
         load.classList.add("hide");
         Lnameerror.innerHTML = "** Lname must be 3 to 20 Character **";
-        return false;
+        // return false;
     }
 
-    if (!isNaN(Lname)) {
+    else if (!isNaN(Lname.value)) {
         load.classList.add("hide");
         Lnameerror.innerHTML = "** Only Character Allow**";
-        return false;
+        // return false;
     }
 
     // username
-    if (Uname == "") {
+    else if (Uname.value == "") {
         load.classList.add("hide");
         Unameerror.innerHTML = "**Plz fill User name Field**";
-        return false;
-    }
-    else if (Uname !== "") {
-        Unameerror.innerHTML = "";
+        // return false;
     }
 
-    if ((Uname.length <= 3) || Uname.length > 20) {
+    else if ((Uname.value.length <= 3) || Uname.value.length > 20) {
         load.classList.add("hide");
         Unameerror.innerHTML = "** User name must be 3 to 20 Character **";
-        return false;
+        // return false;
     }
 
-    if (!isNaN(Uname)) {
+    else if (!isNaN(Uname.value)) {
         load.classList.add("hide");
         Unameerror.innerHTML = "** Only Character Allow**";
-        return false;
+        // return false;
     }
 
     // Email
-    if (Email == "") {
+    else if (Email.value == "") {
         load.classList.add("hide");
         Emailerror.innerHTML = "**Plz fill Email Field**";
-        return false;
-    }
-    else if (Email !== "") {
-        Emailerror.innerHTML = "";
+        // return false;
     }
 
-    if (Email.indexOf('@') <= 0) {
+    else if (Email.value.indexOf('@') <= 0) {
         load.classList.add("hide");
         Emailerror.innerHTML = "** @ at invaild position **";
         return false;
     }
 
-
     // password
 
-    if (password == "") {
+    else if (password.value == "") {
         load.classList.add("hide");
         passworderror.innerHTML = "**Plz fill Password Field**";
         return false;
     }
-    else if (password !== "") {
-        passworderror.innerHTML = "";
-    }
-    if ((password.length <= 3) || password.length > 10) {
+
+    else if ((password.value.length <= 3) || password.value.length > 10) {
         load.classList.add("hide");
         passworderror.innerHTML = "**Password must be greater than 3 digt**";
         return false;
     }
 
-    if (Cpassword == "") {
+    else if (Cpassword.value == "") {
         load.classList.add("hide");
         CpasswordError.innerHTML = "**Plz fill Password Field**";
         return false;
     }
 
-    if (password != Cpassword) {
+    else if (password.value != Cpassword.value) {
         load.classList.add("hide");
         CpasswordError.innerHTML = "** Password not Match**";
         return false;
     }
-    else if (Cpassword !== "") {
-        CpasswordError.innerHTML = "";
-    }
 
     else {
-        setTimeout(function () {
+        fnameerror.innerHTML = ""
+        Lname.innerHTML = ""
+        Uname.innerHTML = ""
+        Email.innerHTML = ""
+        password.innerHTML = ""
+        Cpassword.innerHTML = ""
+
+        function store() {
             var data = {
+
                 FirstName: FirstName,
                 Lname: Lname,
                 Uname: Uname,
@@ -147,37 +132,52 @@ function signUp1() {
                 password: password,
                 Cpassword: Cpassword
             }
+            console.log(data)
             load.classList.add("hide")
             var storage = localStorage.setItem("userInfo", JSON.stringify(data));
-            then(() => {
-                document.getElementById("FirstName").value = "";
-                document.getElementById("LastName").value = "";
-                document.getElementById("UserName").value = "";
-                document.getElementById("Email").value = "";
-                document.getElementById("password").value = "";
-                document.getElementById("Cpassword").value = "";
-                location = "./signUp.html"
-            })
-            
-              
-        }, 300000);
 
 
+        }
 
-        swal(
-            'Good job!',
-            'You clicked the button!',
-            'success'
-          )
-        
-    
+        var div = document.getElementById('pop_success');
+
+
+        // swal(
+        //     'Good job!',
+        //     'You clicked the button!',
+        //     'success'
+        // )
 
     }
+    FirstName.addEventListener('blur', () => {
+        if (FirstName.value.length >= 3)
+            fnameerror.innerHTML = '';
+    })
 
-    
+    Lname.addEventListener('blur', () => {
+        if (Lname.value.length >= 3)
+            Lnameerror.innerHTML = '';
+    })
 
+    Uname.addEventListener('blur', () => {
+        if (Uname.value.length >= 3)
+            Unameerror.innerHTML = '';
+    })
 
+    Email.addEventListener('blur', () => {
+        if (Email.value.length >= 3)
+            Emailerror.innerHTML = '';
+    })
 
+    password.addEventListener('blur', () => {
+        if (password.value, length >= 3)
+            passworderror.innerHTML = '';
+    })
+
+    Cpassword.addEventListener('blur', () => {
+        if (Cpassword.value.length >= 3)
+            CpasswordError.innerHTML = '';
+    })
 
 
 }
