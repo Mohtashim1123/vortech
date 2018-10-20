14c741b180d153a7da44c0f2a0a18238a257efe7
Mohtashim <mouhtashim5@gmail.com>
2018-10-19 23:07:34 -0700

    todo today

--- todomd/custom.js
+++ todomd/custom.js
@@ -3,21 +3,21 @@ let ul = document.getElementById('todo3');
 let Emtyerror = document.getElementById('Emty_error');
 
 var todos = []
-debugger
+
 function addtodo() {
-    debugger
+   
     if (userinput.value == "") {
         Emtyerror.innerHTML = 'You Do not Enter Task';
     }
-  
+
     else {
         todos.push(userinput.value);
         render()
-
+console.log(todos)
     }
-    userinput.addEventListener('blur',() =>{
-        if (userinput.value !=="")
-        Emtyerror.innerHTML='';
+    userinput.addEventListener('blur', () => {
+        if (userinput.value !== "")
+            Emtyerror.innerHTML = '';
     })
 }
 
@@ -26,6 +26,21 @@ function render() {
     items.innerHTML = ''
     for (var i = 0; i <= todos.length - 1; i++) {
 
-        items.innerHTML += `<li>${todos[i]}</li>`
+        items.innerHTML += `<li>${todos[i]}</li>
+        <div class = "btn_parent">
+        <button id="delete" class="btn btn-default" onclick="deleteListElement(${i})">Delete <i class="fa fa-trash"></i></i></button>
+        
+        <button class="btn btn-default">Edit <i class="fa fa-magic ml-1"></i></button>
+        </div>
+`
+       
     }
 }
+
+
+
+function deleteListElement(i) {
+    todos.splice(i, 1);
+    render()
+}
+
