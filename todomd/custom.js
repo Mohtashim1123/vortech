a5fb5af4f3f6668410ce596491b9697e8b09bc16
Mohtashim <mouhtashim5@gmail.com>
2018-10-22 04:19:12 -0700

    Edit function pending

--- todomd/custom.js
+++ todomd/custom.js
@@ -1,23 +1,26 @@
 let userinput = document.getElementById('form122');
 let ul = document.getElementById('todo3');
 let Emtyerror = document.getElementById('Emty_error');
+var edit = false;
 
 var todos = []
 
 function addtodo() {
-   
+
     if (userinput.value == "") {
         Emtyerror.innerHTML = 'You Do not Enter Task';
+        Emtyerror.className = 'Error_text';
     }
 
     else {
         todos.push(userinput.value);
         render()
-console.log(todos)
+        console.log(todos)
     }
     userinput.addEventListener('blur', () => {
         if (userinput.value !== "")
             Emtyerror.innerHTML = '';
+            Emtyerror.className = '';
     })
 }
 
@@ -25,15 +28,24 @@ function render() {
     var items = document.getElementById('todolist');
     items.innerHTML = ''
     for (var i = 0; i <= todos.length - 1; i++) {
-
-        items.innerHTML += `<li>${todos[i]}</li>
-        <div class = "btn_parent">
-        <button id="delete" class="btn btn-default" onclick="deleteListElement(${i})">Delete <i class="fa fa-trash"></i></i></button>
+      
+        items.innerHTML += `
         
-        <button class="btn btn-default">Edit <i class="fa fa-magic ml-1"></i></button>
-        </div>
-`
+            
        
+            <li  name = ${i}>${todos[i]}</li>
+            <div  class = "btn_parent test">
+            <button id="delete" class="btn btn-default" onclick="deleteListElement(${i})">Delete <i class="fa fa-trash"></i></i></button>
+            <button id = "edit" class="btn btn-default" onclick = "edit1(${i})">Edit <i class="fa fa-magic ml-1"></i></button>
+            </div>
+        
+       
+        `
+ //  <li>${todos[i]}</li>
+        // <div class = "btn_parent">
+        // <button id="delete" class="btn btn-default" onclick="deleteListElement(${i})">Delete <i class="fa fa-trash"></i></i></button>
+        // <button id = "edit" class="btn btn-default" onclick = "edit1()">Edit <i class="fa fa-magic ml-1"></i></button>
+        // </div>`
     }
 }
 
@@ -42,5 +54,19 @@ function render() {
 function deleteListElement(i) {
     todos.splice(i, 1);
     render()
+   
+}
+
+
+function edit1(i)
+{console.log(i)
+    
+  var hel =  document.getElementById("test")
+
+    // document.getElementsByClassName('test').s
+   
+    // edit = !edit
+    render()
+    
 }
 
