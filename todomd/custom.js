3d65f69dd317100d9ed346810f0840f43bd16503
Mohtashim <mouhtashim5@gmail.com>
2018-10-26 03:12:46 -0700

    Todo Finaly Done

--- todomd/custom.js
+++ todomd/custom.js
@@ -4,112 +4,76 @@ let EmtyError = document.getElementById('Emty_error');
 
 let todos = []
 
-function addTodo(){
-    const{value} = userInput;
-    if(userInput.value === ''){
-        EmtyError.innerHTML  = `<div class = "alert alert-danger" role= "alert">
-        You do not enter task
-        </div>`;
-        userInput.addEventListener('blur', () =>{
-            if (userInput.value !== ''){
-                EmtyError.innerHTML='';
-            }
+function addTodo() {
+    const { value } = userInput;
+    if (userInput.value === '') {
+        EmtyError.innerHTML = `
+        <div class = "alert alert-danger" role= "alert">
+            You do not enter task
+        </div>
+        `;
+        userInput.addEventListener('blur', () => {
+            if (userInput.value !== '')
+                EmtyError.innerHTML = '';
         })
-    }else{
+    } else {
         todos.push(userInput.value);
         renderItem(value);
     }
 }
+
 /**
- * @param String text
+ * @param {Element} element
+ * @param {String} text
  */
-function renderItem(text){
+function renderItem(text) {
+    const id = new Date().getMilliseconds()
     ul.innerHTML += `
-    <li data-set-mode = "false" id = "${new Date().getMilliseconds()}">
-    <p>${text}</p>
-    `
+    <li data-set-mode = "false" id = "${id}">
+        <p>${text}</p>
+        <button type="button" class="btn btn-primary" onclick="onEdit(${id})" style="width: 95px">Edit</button>
+        <button type="button" class="btn btn-danger" onclick = "remove(this.parentElement.id)">Delete</button>
+    </li>
+    `;
 }
 
-// function addTodo() {
-//     const { value } = userInput;
-//     if (userInput.value === '') {
-//         console.log(userInput)
-//         EmtyError.innerHTML = `<div class="alert alert-danger" role="alert">
-//         You Do Not Enter Task
-//       </div>`;
-//         userInput.addEventListener('blur', () => {
-//             if (userInput.value !== '') {
-//                 EmtyError.innerHTML = '';
-//             }
-//         })
-//     } else {
-//         todos.push(value);
-//         renderItem(value);
-//     }
-// }
+/**
+ * @param {Number} id   
+ */
+function remove(id) {
+    document.getElementById(id).remove();
+}
 
-// /**
-//  * @param {Number} id 
-//  */
-// function remove(id) {
-//     document.getElementById(id).remove();
-    
-// }
+/**
+ * @param {Element} element
+ */
+function onEdit(id) {
+    const li = document.getElementById(id);
+    li.dataset.editMode = !li.dataset.editMode;
 
-// /**
-//  * 
-//  * @param {Element} element 
-//  * @param { String } text
-//  */
+    if (li.dataset.editMode) {
+        const p = li.querySelector('p');
+        const text = p.innerHTML;
+        li.outerHTML = `
+        <li id="${id}" data-set-mode="true">
+            <input class="form-control EditForm form-control-lg" type="text" value="${text}" placeholder="Typing..."> 
+            <button type="button" class="btn btn-success" onclick = "modify(this.previousElementSibling.value, ${id})">Update</button>
+            <button type="button" class="btn btn-danger" onclick = "remove(this.parentElement.id)">Delete</button>
+        </li>
+        `;
+    }
+}
 
-// function renderItem(text) {
-    
-//     ul.innerHTML += 
-//     `
-//         <li data-edit-mode="false" id="${new Date().getMilliseconds()}">
-//             <p>${text}</p>
-//             <button onclick="onEdit(this)">Edit</button>
-//             <button onclick="remove(this.parentElement.id)">Delete</button>
-//         </li>
-//     `;
-    
-// }
-
-// /**
-//  * 
-//  * @param { String } text
-//  * @param { Number } id
-//  */
-// function modify(text, id) {
-//     document.getElementById(id).innerHTML = `
-//         <li data-edit-mode="false" id="${id}">
-//             <p>${text}</p>
-//             <button onclick="onEdit(this)">Edit</button>
-//             <button onclick="remove(this)">Delete</button>
-//         </li>
-//     `;
-   
-// }
-
-// /**
-//  * 
-//  * @param {Element} element 
-//  */
-// function onEdit(element) {
-//     const parent = element.parentElement;
-//     element.parentElement.dataset.editMode = !element.parentElement.dataset.editMode;
-  
-
-//     if (element.parentElement.dataset.editMode) {
-//         const p = parent.querySelector('p');
-//         const text = p.innerHTML;
-//         parent.innerHTML = `
-//                     <li id="${parent.id}">
-//                         <input type="text" value="${text}" />
-//                         <button onclick="modify(this.previousElementSibling.value, this.parentElement.id)">Modify</button>
-//                     </li>
-//         `;
-        
-//     }
-// }
+/**
+ * @param {String} text
+ * @param {id} Number
+ */
+function modify(text, id) {
+    document.getElementById(id).innerHTML = " ";
+    document.getElementById(id).innerHTML += `
+        <p>${text}</p>
+        <button type="button" class="btn btn-primary" onclick="onEdit(${id})" style="width: 95px">Edit</button>
+        <button type="button" class="btn btn-danger" onclick = "remove(this.parentElement.id)">Delete</button>
+    `;
+}
 
